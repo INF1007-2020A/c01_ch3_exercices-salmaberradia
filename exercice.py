930ef18df4ab991b1af41f538a47cdad11a2821e
salmaberradia <salmaberradia@hotmail.com>
2020-09-14 18:02:53 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -6,7 +6,7 @@ import math
 
 
 def average(a: 4.2, b: 6.5, c: 10.1) -> float:
-    return 
+    return (a+b+c)
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
