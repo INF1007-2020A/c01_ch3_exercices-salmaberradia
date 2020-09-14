00b4ad8fa0a90ddacd942ae486831aeebad9d005
salmaberradia <salmaberradia@hotmail.com>
2020-09-14 18:30:50 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,7 +15,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 def to_degrees(angle_rads: float) -> tuple:
 deg = angle_degs
-min = 
+min = ang_rad
 sec = 
     return 0, 0, 0.0
 
